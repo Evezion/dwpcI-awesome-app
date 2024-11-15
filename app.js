3ea1ae325cdafd34ed2597ebdbbd718d9c259b37
Evezion <159814619+Evezion@users.noreply.github.com>
2024-11-15 15:31:29 +0000

    🥧 Filtrado de Rutas

--- app.js
+++ app.js
@@ -4,20 +4,18 @@ import express from 'express';
 import httpStatus from 'http-status';
 
 // Importando el enrutador
-import adminRouter from './routes/admin.routes.js';
-import shopRouter from './routes/shop.routes.js';
+import adminRouter from './routes/admin.route.js';
+import shopRouter from './routes/shop.route.js';
 
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
 
 // Se registra el middleware del body-parser
-
 app.use(express.urlencoded({ extended: true }));
 
-
 // Se agrega ruta de administrador
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 // Se agrega ruta shop
 app.use(shopRouter);
 
@@ -28,12 +26,10 @@ app.use((req, res, next) => {
   .send("<h1 style='color: crimson;'>🤷‍♂️ Not found 🤷‍♂️</h1>")
 });
 
-
-
 // Definiendo puertos
 const port = 3000;
 const ip = "0.0.0.0"
 
 // Arrancando el servidor
 app.listen(port, ip, () => {
-  console.log(`🤖 Sirviendo en http://localhost:${port}`)});
+  console.log(`🤖 Sirviendo en http://localhost:${port}`);});
