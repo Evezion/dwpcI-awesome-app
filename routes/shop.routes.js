9f63034545b5e1afc4924d952e72d5d000bcc91d
Evezion <159814619+Evezion@users.noreply.github.com>
2024-12-08 03:38:16 +0000

    Entrega de listas

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -1,30 +1,30 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-
-// Importando el gestor de rutas
-import path from 'path';
+// Importando el arreglo de productos
+import { products } from './admin.route.js';
 
 // Creando una instancia del enrutador de express
 const router = Router();
-
-// Importando productos
-import { products } from './admin.routes.js';
 
 // GET /
 router.get('/', (req, res)=>{
   // Mostrando productos en memoria
   console.log(products);
   console.log("📢 Sirviendo la ruta '/'");
-  res.render('shop', {shop: 'active', docTitle:"Shop"});
+  res.render('shop', { 
+    shop: 'active', 
+    docTitle:"Shop",
+    viewStyle: '/css/product.css',
+    products
+  });
 });
 
 // GET /about
 router.get('/about', (req, res) => {
   console.log("📢 Sirviendo la ruta '/about'");
-  // Se contesta al sever
+  // Se contesta al server
   res.send(`
     <h1>🪄 About...</h1>
     <p>App for Fullstack Web Dev Course I!</p>
   `);
 });
-
